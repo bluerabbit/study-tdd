42bb624e9b52b60820f456225ce64bc2fd0e12cd
Akira Kusumoto <akirakusumo10@gmail.com>
2013-06-24 19:24:08 +0900

    Add rank method

--- Gemfile
+++ Gemfile
@@ -2,6 +2,7 @@
 source 'https://rubygems.org'
 
 gem 'rake', '0.9.2'
+gem 'activesupport'
 
 group :test do
   gem 'rspec', '2.10.0'
--- lib/ranking.rb
+++ lib/ranking.rb
@@ -1,18 +1,20 @@
 # coding: utf-8
 require 'logger'
+require 'active_support/all'
 
 class Ranking
-  class << self
-    def logger
-      logger ||= Logger.new(File.open('log/ranking.log', 'a'))
-    end
-    def foo
-      logger.info(111)
-      'foo'
-    end
+  @@logger = Logger.new(File.open('log/ranking.log', 'a'))
+
+  attr_reader :scores
+
+  def initialize(*scores)
+    @scores = scores
   end
 
-  def fuga
-    'fuga'
+  def rank(score)
+    @scores << score
+    @@logger.info "arg score:#{score}"
+    @@logger.info "scores:#{@scores}"
+    1
   end
 end
